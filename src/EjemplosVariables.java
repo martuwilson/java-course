933cee98ae9a63a87d1dd480e0d0cf2ccfdc3422
martuwilson <williner.dev@gmail.com>
2025-10-05 12:48:41 -0300

    inicio JAVA

---
+++ src/EjemplosVariables.java
@@ -0,0 +1,28 @@
+public class EjemplosVariables {
+    public static void main(String[] args) {
+        // Variable string
+        //String saludar = "Hola Mundo con JAVA desde variable";
+        //String saludarMayuscula = saludar.toUpperCase();
+
+        //System.out.println(saludar + " y sume este texto concatenado");
+        //System.out.println("--- --- --- --- --- --- ---");
+        //System.out.println(saludarMayuscula);
+
+        //Integer numero = 10;
+        //Si es Integer y no int puede tener metodos dentro
+        //int numero = 10;
+        //System.out.println(numero);
+        //int edadUser = 29;
+        //boolean valor = true;
+        //String nombre;
+        /*if(valor){
+            System.out.println("El valor es verdadero");
+            nombre = "Martin";
+            System.out.println(nombre);
+        }else{
+            System.out.println("Es valor en falso");
+        }*/
+    }
+
+
+}
---
+++ src/VariablesPrimitivas.java
@@ -0,0 +1,8 @@
+public class VariablesPrimitivas {
+    public static void main(String[] args) {
+    // Enteros
+
+        byte numeroByte = 7; //maximo valor -127 a 127 almacenables en 8bits
+        System.out.println("Numero byte: " + numeroByte);
+    }
+}
